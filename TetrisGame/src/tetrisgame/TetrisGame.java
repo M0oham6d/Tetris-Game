7a04739847c360a04397da3410c7d2f235f792d0
Muhammed Ayman <Mo321343@gmail.com>
2023-05-03 12:58:08 +0300

    Add files via upload

---
+++ TetrisGame/src/tetrisgame/TetrisGame.java
@@ -0,0 +1,42 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/javafx/FXMain.java to edit this template
+ */
+package tetrisgame;
+
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+/**
+ *
+ * @author Mo321
+ */
+public class TetrisGame extends Application {
+    
+    @Override
+    public void start(Stage primaryStage) {
+        Button btn = new Button("Tetris Game");
+        
+        StackPane root = new StackPane();
+        root.getChildren().add(btn);
+        
+        Scene scene = new Scene(root, 300, 250);
+        
+        primaryStage.setTitle("Tetris Game");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        launch(args);
+    }
+    
+}
