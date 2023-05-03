d5e716ba583a20e7f4ecd46ec7b7aadce1d465f2
Muhammed Ayman <Mo321343@gmail.com>
2023-05-03 16:00:06 +0300

    Add files via upload

--- TetrisGame/src/tetrisgame/TetrisGame.java
+++ TetrisGame/src/tetrisgame/TetrisGame.java
@@ -5,6 +5,7 @@
 package tetrisgame;
 
 import javafx.application.Application;
+import static javafx.application.Application.launch;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.scene.Scene;
